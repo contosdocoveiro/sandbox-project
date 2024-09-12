b6fcc25b6f916b05fb487082fae6e5b673af6914
Brunno Toscano Messias <brunnotm23@gmail.com>
2024-09-12 11:39:14 -0300

    sistema de luz implementado

--- scripts/global/hurtbox.gd
+++ scripts/global/hurtbox.gd
@@ -12,7 +12,7 @@ func _ready() -> void:
 func _on_area_entered(hitbox: Area2D) -> void:
 	if hitbox == null:
 		return
-	if hitbox.owner == owner:
+	if hitbox.owner == owner or hitbox.owner.owner == owner:
 		return
 	
 	if owner.has_method("take_damage"):
